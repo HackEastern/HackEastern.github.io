e1cf0acc47aeba7ba51dab8a4a8eb19e53d993ae
Zac Collins <zcollins@sscorp.com>
2016-11-16 00:41:47 -0500

    Updates to personal project using alias

--- js/countdown.js
+++ js/countdown.js
@@ -1,15 +1,60 @@
-//100% vegetarian code
+
 function startCountdown() {
-    //Dates are declared in the format year, month, day, hours, minutes, seconds, milliseconds
     var hackathonDate = new Date("December 3, 2016 00:00:00");
-    var h = hackathonDate.getHours();
-    var m = hackathonDate.getMinutes();
-    var s = hackathonDate.getSeconds();
-    m = padTime(m);
-    s = padTime(s);
-    document.getElementById('txt').innerHTML = hackathonDate;
-    h + ":" + m + ":" + s;
+    var today = new Date();
+    console.log(getDaysTill(hackathonDate));
+    var daysLeft    = today.getDate()    - hackathonDate.getDate();
+    var hoursLeft   = today.getHours()   - hackathonDate.getHours();
+    var minutesLeft = today.getMinutes() - hackathonDate.getMinutes();
+    var secondsLeft = today.getSeconds() - hackathonDate.getSeconds();
+
+    m = padTime(minutesLeft);
+    s = padTime(secondsLeft);
+    document.getElementById('txt').innerHTML =
+        "Days left: " + daysLeft
+        +
+        "<br />"
+        +
+        "Hours left: " + hoursLeft
+        +
+        "<br />"
+        +
+        "Minutes left: " + minutesLeft
+        +
+        "<br />"
+        +
+        "Seconds left: " + secondsLeft;
+
     var t = setTimeout(startCountdown, 500);
+}
+
+/**
+* Grab the days until the given date object and return. *Only works for up to a year away*
+* @param {hackathonDate} hackathon date
+* @return {daysTill} number of days until the hackathon date
+*/
+function getDaysTill(hackathonDate) {
+    var currDate = new Date();
+    var currMonth = currDate.getMonth()+1;
+    var hackathonMonth = hackathonDate.getMonth()+1;
+
+    //initialize days till with the days until the end of this month
+    var daysTill = new Date(currDate.getFullYear(), currMonth, 0).getDate() - currDate.getDate();
+
+    //increment month because we just added those days
+    currMonth++;
+
+    //While less than hackathon month, grab days in the month, add to total, increment currMonth
+    while(currMonth < hackathonMonth) {
+      daysInMonth = new Date(currDate.getFullYear(), currMonth, 0).getDate();
+      daysTill += daysInMonth;
+      currMonth++;
+    }
+
+    //Add the date inside the hackathon month to running total
+    daysTill += hackathonDate.getDate();
+
+    return daysTill;
 }
 
 function padTime(i) {
